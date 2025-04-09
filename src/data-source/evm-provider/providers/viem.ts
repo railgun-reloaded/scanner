d39bb61b04de21e0ed927f682f2e5bb78868d2cb
zy0n.bear <zy0n@users.noreply.github.com>
2025-04-09 01:07:47 -0400

    feat: implement ViemProvider for EVM interactions and add comprehensive tests

---
+++ src/data-source/evm-provider/providers/viem.ts
@@ -0,0 +1,272 @@
+import EventEmitter from 'node:events'
+
+import type { PublicClient } from 'viem'
+import { createPublicClient, getContract, http, webSocket } from 'viem'
+import { arbitrum, bsc, mainnet, polygon } from 'viem/chains'
+
+import { delay, promiseTimeout } from '../../utils'
+
+const SCAN_CHUNKS = 1000n
+const EVENTS_SCAN_TIMEOUT = 5000
+const SCAN_TIMEOUT_ERROR_MESSAGE = 'getLogs request timed out after 5 seconds.'
+const RAILGUN_SCAN_START_BLOCK = 14693000n
+const RAILGUN_SCAN_START_BLOCK_V2 = 16076000n
+
+/**
+ *   ViemProvider
+ */
+class ViemProvider<T = any> extends EventEmitter implements AsyncIterable<T> {
+  /**
+   *  provider URL
+   */
+  url: string
+  /**
+   * last scanned block
+   */
+  lastScannedBlock: bigint
+  /**
+   * syncing status
+   */
+  syncing: boolean
+  /**
+   * start block
+   */
+  startBlock: bigint
+  /**
+   * provider
+   */
+  private provider?: PublicClient
+  /**
+   *  contract
+   */
+  contract
+  /**
+   * event queue
+   */
+  private eventQueue: T[] = []
+
+  /**
+   *  initialized status
+   */
+  initialized: boolean
+
+  /**
+   * initialized promise
+   */
+  initializedPromise: any
+
+  /**
+   * initialized result
+   */
+  initializedResolve: any
+  /**
+   * contract abi
+   */
+  abi: any
+
+  /**
+   * unwatch blocks
+   */
+  unwatchBlocks?: () => void
+  /**
+   * unwatch contract events
+   */
+  unwatchContractEvents?: () => void
+  /**
+   * constructor for EthersProvider
+   * @param url - The provider URL
+   * @param address - The contract address
+   * @param abi - The contract ABI
+   * @param options - Options for the provider
+   * @param options.ws - Whether to use WebSocket or not
+   * @param options.chainId - The chain ID
+   * @param options.transportConfig - The transport config for the provider
+   */
+  constructor (url: string, address: `0x${string}`, abi: any, options: { chainId: number, ws?: boolean, transportConfig?: any /* add in more options later */ }) {
+    super()
+    // Initialization code if needed
+    this.url = url
+    this.abi = abi
+    this.lastScannedBlock = BigInt(0)
+    this.syncing = false
+    this.startBlock = BigInt(0)
+    const network = this.chainIdToNetwork(options.chainId)
+
+    // cons
+    this.provider = createPublicClient({
+      transport:
+        options.ws ? webSocket(url, options.transportConfig) : http(url, options.transportConfig),
+      chain: network,
+    })
+
+    this.contract = getContract({
+      address,
+      abi,
+      client: this.provider,
+    })
+    this.initialized = false
+    this.initializedPromise = new Promise((resolve) => {
+      this.initializedResolve = resolve
+      this.setupListeners()
+    })
+  }
+
+  /**
+   * Get the chain ID from the provider
+   * @param chainId - The chain ID
+   * @returns - The network object
+   */
+  chainIdToNetwork (chainId: number) {
+    switch (chainId) {
+      case 1:
+        return mainnet
+      case 5:
+        return arbitrum
+      case 56:
+        return bsc
+      case 137:
+        return polygon
+      default:
+        throw new Error(`Unsupported chain ID: ${chainId}`)
+    }
+  }
+
+  /**
+   *  Start iterating from a given height.
+   */
+  private async setupListeners () {
+    // Logic to set up listeners for the provider
+    // this.provider.on('pending', (tx) => {
+    //   // TODO: Handle pending transaction event
+    //   console.log('Pending transaction:', tx)
+    // })
+    if (!this.provider) {
+      throw new Error('Provider is not initialized')
+    }
+
+    // returns a destroy function
+    this.unwatchBlocks = this.provider.watchBlocks({
+
+      /**
+       * Called when a new block is mined.
+       * @param block - The block object
+       */
+      onBlock: (block) => {
+        this.emit('newHead', block)
+      }
+    })
+
+    this.unwatchContractEvents = this.provider.watchContractEvent({
+      address: this.contract.address,
+      abi: this.abi,
+      /**
+       *  Called when a new block is mined.
+       * @param logs - The logs from the new block
+       */
+      onLogs: (logs) => {
+        this.eventQueue.push(logs as unknown as T)
+        // this.emit('event', logs)
+      }
+    })
+
+    this.initialized = true
+    this.syncing = true
+    this.initializedResolve()
+    // handle railgun
+  }
+
+  /**
+   *  Get the provider
+   *  @returns - The provider instance
+   */
+  getProvider () {
+    // Logic to get the provider
+    return this.provider
+  }
+
+  /**
+   * Await initialization of the provider.
+   */
+  async awaitInitialized () {
+    if (!this.initialized) {
+      await this.initializedPromise
+    }
+  }
+
+  /**
+   * Start iterating events.
+   * @returns - An async generator that yields events
+   * @throws - Error if the source is not a ReadableStream or an AsyncIterable
+   * @yields T - The data read from the source
+   */
+  async * [Symbol.asyncIterator] (): AsyncGenerator<T> {
+    while (true) {
+      if (this.eventQueue.length > 0) {
+        yield this.eventQueue.shift() as T
+      } else {
+        // TODO: return undefined ONLY FOR TESTING
+        if (this.syncing) {
+          await new Promise(resolve => setTimeout(resolve, 1000)) // Wait for 1 second before checking again
+        } else { return }
+      }
+    }
+  }
+
+  /**
+   *  Start iterating from a given height.
+   * @param options - Options for the iterator
+   * @param options.startBlock - The block number to start from
+   * @param options.endBlock - The block number to end at
+   * @returns - An async generator that yields events
+   * @yields T - The data read from the source
+   */
+  async * from (options:{ startBlock: bigint, endBlock: bigint }) {
+    const { startBlock, endBlock } = options
+    this.startBlock = BigInt(startBlock)
+    // this.lastScannedBlock = BigInt(endBlock)
+    // Logic to iterate from a given height
+    // const TOTAL_BLOCKS = BigInt(endBlock) - BigInt(startBlock)
+    let currentOffset = startBlock
+    while (true) {
+      if (currentOffset >= endBlock) {
+        break
+      }
+      const startChunk = currentOffset
+      if (!this.provider) {
+        throw new Error('Provider is not initialized')
+      }
+      const events = await promiseTimeout(
+        this.provider.getContractEvents({
+          address: this.contract.address,
+          abi: this.abi,
+          fromBlock: startChunk,
+          toBlock: startChunk + SCAN_CHUNKS,
+        }),
+        EVENTS_SCAN_TIMEOUT,
+        SCAN_TIMEOUT_ERROR_MESSAGE
+      )
+      yield events
+      currentOffset += SCAN_CHUNKS + 1n
+      this.lastScannedBlock = currentOffset - 1n// scan is inclusive
+      if (currentOffset > endBlock) {
+        currentOffset = endBlock
+      }
+      await delay(250) // Delay to avoid rate limiting, can avoid this with forking...
+    }
+  }
+
+  /**
+   * Destroy the provider
+   * @returns - A promise that resolves when the provider is destroyed
+   */
+  async destroy () {
+    // Logic to destroy the provider and iterators
+    this.unwatchBlocks?.()
+    this.unwatchContractEvents?.()
+    this.syncing = false
+    // provider doesnt supply any destroy methods?
+    delete this.provider
+  }
+}
+
+export { ViemProvider, RAILGUN_SCAN_START_BLOCK, RAILGUN_SCAN_START_BLOCK_V2 }
---
+++ test/src/data-source/evm-provider/providers/viem.test.ts
@@ -0,0 +1,120 @@
+import { test } from 'brittle'
+import dotenv from 'dotenv'
+
+import { ABIRailgunSmartWallet } from '../../../../../src/abi/abi'
+import { RAILGUN_SCAN_START_BLOCK_V2, ViemProvider } from '../../../../../src/data-source/evm-provider/providers/viem'
+
+dotenv.config()
+
+const TEST_RPC_URL = process.env['TEST_RPC_URL_HTTPS']
+const TEST_RPC_URL_WSS = process.env['TEST_RPC_URL_WSS']
+
+test('Viem-Provider:iterator https', async (t) => {
+  // setup
+  const TEST_CONTRACT_ADDRESS = '0xFA7093CDD9EE6932B4eb2c9e1cde7CE00B1FA4b9'
+
+  if (typeof TEST_RPC_URL === 'undefined') {
+    t.fail('TEST_RPC_URL is not set')
+  }
+  const provider = new ViemProvider(
+    TEST_RPC_URL!,
+    TEST_CONTRACT_ADDRESS,
+    ABIRailgunSmartWallet,
+    { chainId: 1, ws: false }
+  )
+  await provider.initializedPromise
+
+  provider.on('newHead', (block) => {
+    t.pass(`http:from New block: ${block}`)
+  })
+  setTimeout(async () => {
+    await provider.destroy()
+  }, 20_000)
+  for await (const event of provider) {
+    t.pass(`http:from FoundEvent: ${event.fragment.name}`)
+  }
+})
+
+test('Viem-Provider:iterator wss', async (t) => {
+  // setup
+  const TEST_CONTRACT_ADDRESS = '0xFA7093CDD9EE6932B4eb2c9e1cde7CE00B1FA4b9'
+
+  if (typeof TEST_RPC_URL_WSS === 'undefined') {
+    t.fail('TEST_RPC_URL_WSS is not set')
+  }
+  const provider = new ViemProvider(
+    TEST_RPC_URL_WSS!,
+    TEST_CONTRACT_ADDRESS,
+    ABIRailgunSmartWallet,
+    { chainId: 1, ws: true }
+  )
+  await provider.initializedPromise
+  provider.on('newHead', (block) => {
+    t.pass(`wss:from New block: ${block}`)
+  })
+  setTimeout(async () => {
+    await provider.destroy()
+  }, 20_000)
+  for await (const event of provider) {
+    // console.log('FoundEvent', event.fragment.name)
+    t.pass(`wss:from FoundEvent: ${event.fragment.name}`)
+  }
+})
+
+test('Viem-Provider:from https with scanOptions', async (t) => {
+  t.timeout(120_000)
+  // setup
+  const TEST_CONTRACT_ADDRESS = '0xFA7093CDD9EE6932B4eb2c9e1cde7CE00B1FA4b9'
+
+  if (typeof TEST_RPC_URL === 'undefined') {
+    t.fail('TEST_RPC_URL is not set')
+  }
+  const provider = new ViemProvider(
+    TEST_RPC_URL!,
+    TEST_CONTRACT_ADDRESS,
+    ABIRailgunSmartWallet,
+    { chainId: 1, ws: false }
+  )
+  await provider.initializedPromise
+
+  provider.on('newHead', (block) => {
+    t.pass(`https:from New block: ${block}`)
+  })
+  const scanOptions = {
+    startBlock: RAILGUN_SCAN_START_BLOCK_V2,
+    endBlock: RAILGUN_SCAN_START_BLOCK_V2 + 50_000n,
+  }
+  for await (const event of provider.from(scanOptions)) {
+    t.pass(`wss:from FoundEvents: ${event.length}`)
+  }
+  await provider.destroy()
+  console.log('destroyed')
+})
+
+test('Viem-Provider:from wss with scanOptions', async (t) => {
+  t.timeout(120_000)
+  // setup
+  const TEST_CONTRACT_ADDRESS = '0xFA7093CDD9EE6932B4eb2c9e1cde7CE00B1FA4b9'
+
+  if (typeof TEST_RPC_URL_WSS === 'undefined') {
+    t.fail('TEST_RPC_URL_WSS is not set')
+  }
+  const provider = new ViemProvider(
+    TEST_RPC_URL_WSS!,
+    TEST_CONTRACT_ADDRESS,
+    ABIRailgunSmartWallet,
+    { chainId: 1, ws: true }
+  )
+  await provider.initializedPromise
+  provider.on('newHead', (block) => {
+    t.pass(`wss:from New block: ${block}`)
+  })
+  const scanOptions = {
+    startBlock: RAILGUN_SCAN_START_BLOCK_V2,
+    endBlock: RAILGUN_SCAN_START_BLOCK_V2 + 50_000n,
+  }
+  for await (const event of provider.from(scanOptions)) {
+    t.pass(`wss:from FoundEvents: ${event.length}`)
+  }
+  await provider.destroy()
+})
