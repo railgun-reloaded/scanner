92584b530ecdd51e0339c4c55cecff953c3f0a1c
zy0n.bear <zy0n@users.noreply.github.com>
2025-04-28 15:04:44 -0400

    refactor: integrate GraphProvider and remove SubsquidProvider, update package dependencies

---
+++ src/data-source/graph-provider.ts
@@ -0,0 +1,21 @@
+/**
+ * The GraphProvider class is a data source provider that interacts with the Subsquid API.
+ * It provides methods to fetch data from the Subsquid API and process it for use in the application.
+ */
+class GraphProvider {
+  /**
+   * The GraphProvider constructor initializes the provider.
+   */
+  constructor () {
+    this.initialize()
+  }
+
+  /**
+   * - The GraphProvider constructor initializes the provider.
+   */
+  async initialize () {
+    // Initialize the provider
+  }
+}
+
+export { GraphProvider }
--- src/data-source/subsquid-provider.ts
+++
@@ -1,9 +0,0 @@
-/**
- * The SubsquidProvider class is a data source provider that interacts with the Subsquid API.
- * It provides methods to fetch data from the Subsquid API and process it for use in the application.
- */
-class SubsquidProvider {
-
-}
-
-export { SubsquidProvider }
