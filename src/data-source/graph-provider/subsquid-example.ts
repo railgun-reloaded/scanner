66c4676faab2251def92c82a5c5b3bcc27e80991
zy0n.bear <zy0n@users.noreply.github.com>
2025-04-28 16:02:31 -0400

    ding

--- src/data-source/graph-provider/subsquid-example.ts
+++ src/data-source/graph-provider/subsquid-example.ts
@@ -1,11 +1,92 @@
 import { SubsquidClient } from '@railgun-reloaded/subsquid-client'
+import { CommitmentOrderByInput } from '@railgun-reloaded/subsquid-client/src/generated/types'
 import { SUPPORTED_NETWORKS } from '@railgun-reloaded/subsquid-client/src/networks'
 // example output: ['ethereum', 'ethereumSepolia', 'bsc', '
 
 console.log(SUPPORTED_NETWORKS)
 
 // Initialize the client with a predefined network
+/**
+ * Get all shields
+ * @returns -{ tokens: any[] }
+ */
+const getAllShields = async () => {
+  const client = new SubsquidClient({ network: 'ethereum' })
 
+  const { commitments } = await client.query(
+    {
+      commitments: {
+        orderBy: [CommitmentOrderByInput.BlockNumberAsc, CommitmentOrderByInput.TreePositionAsc],
+        where: { blockNumber_gte: '$blockNumber' },
+        limit: 10000,
+        fields: [
+          'id',
+          'treeNumber',
+          'batchStartTreePosition',
+          'treePosition',
+          'blockNumber',
+          'transactionHash',
+          'blockTimestamp',
+          'commitmentType',
+          'hash',
+        ],
+        on: {
+          // LegacyGeneratedCommitment: [
+          //   'id',
+          //   'treeNumber',
+          //   'batchStartTreePosition',
+          //   'treePosition',
+          //   'blockNumber',
+          //   'transactionHash',
+          //   'blockTimestamp',
+          //   'commitmentType',
+          //   'hash',
+          //   'encryptedRandom',
+          //   `preimage {
+          //     id
+          //     npk
+          //     value
+          //     token {
+          //       id
+          //       tokenType
+          //       tokenSubID
+          //       tokenAddress
+          //     }
+          //   }`,
+          // ],
+          LegacyEncryptedCommitment: [
+            'id',
+            'blockNumber',
+            'blockTimestamp',
+            'transactionHash',
+            'treeNumber',
+            'batchStartTreePosition',
+            'treePosition',
+            'commitmentType',
+            'hash',
+            // here’s the alias + nested object exactly as you wrote it:
+            'legacyCiphertext: ciphertext {',
+            '  id',
+            '  ciphertext {',
+            '    id',
+            '    iv',
+            '    tag',
+            '    data',
+            '  }',
+            '  ephemeralKeys',
+            '  memo',
+            '}',
+          ],
+        },
+      },
+    },
+    {
+      operationName: 'Commitments',
+      variables: [{ name: 'blockNumber', type: 'BigInt', default: 0 }],
+    }
+  )
+  return commitments
+}
 /**
  *  Example usage of the SubsquidClient
  * @returns -{ tokens: any[] }
@@ -13,16 +94,17 @@ console.log(SUPPORTED_NETWORKS)
 const test = async () => {
 // Or initialize with a custom Subsquid URL
   // const customClient = new SubsquidClient({ customSubsquidUrl: 'https://my-subsquid-api.example.com/graphql' })
-  const client = new SubsquidClient({ network: 'ethereum' })
 
   // Simple query for tokens with type-safety
-  const { tokens } = await client.query({
-    tokens: {
-      fields: ['id', 'tokenType', 'tokenAddress', 'tokenSubID'],
-      limit: 5
-    }
-  })
-  return tokens
+  // const { tokens } = await client.query({
+  //   tokens: {
+  //     fields: ['id', 'tokenType', 'tokenAddress', 'tokenSubID'],
+  //     limit: 5
+  //   }
+  // })
+  const shields = await getAllShields()
+
+  return { shields }
 }
 test().then(e => {
   console.log(e)
--- src/data-source/graph-provider/subsquid.ts
+++ src/data-source/graph-provider/subsquid.ts
@@ -1,87 +1,87 @@
-import EventEmitter from 'node:events'
+// import EventEmitter from 'node:events'
 
-// import type { NetworkName } from '../../../src/globals'
-// import { delay, getAbiForNetworkBlockRange, promiseTimeout } from '../../utils'
+// // import type { NetworkName } from '../../../src/globals'
+// // import { delay, getAbiForNetworkBlockRange, promiseTimeout } from '../../utils'
 
-// const SCAN_CHUNKS = 50_000n // need to categorize this by provider, they each have their own limits. 500 is base low. we can attempt to just 'find it' but this can also incur 'ratelimits' that will effect the calculation 'guestimate' of this value.
-// const EVENTS_SCAN_TIMEOUT = 20_000
-// const SCAN_TIMEOUT_ERROR_MESSAGE = 'getLogs request timed out after 5 seconds.'
-const RAILGUN_SCAN_START_BLOCK = 14693000n
-const RAILGUN_SCAN_START_BLOCK_V2 = 16076000n
+// // const SCAN_CHUNKS = 50_000n // need to categorize this by provider, they each have their own limits. 500 is base low. we can attempt to just 'find it' but this can also incur 'ratelimits' that will effect the calculation 'guestimate' of this value.
+// // const EVENTS_SCAN_TIMEOUT = 20_000
+// // const SCAN_TIMEOUT_ERROR_MESSAGE = 'getLogs request timed out after 5 seconds.'
+// const RAILGUN_SCAN_START_BLOCK = 14693000n
+// const RAILGUN_SCAN_START_BLOCK_V2 = 16076000n
 
-/**
- *   SubsquidProvider
- */
-class SubsquidProvider<T = any> extends EventEmitter implements AsyncIterable<T> {
-  /**
-   * constructor for SubsquidProvider
-   */
-  constructor () {
-    super()
-    this.setupListeners()
-  }
+// /**
+//  *   SubsquidProvider
+//  */
+// class SubsquidProvider<T = any> extends EventEmitter implements AsyncIterable<T> {
+//   /**
+//    * constructor for SubsquidProvider
+//    */
+//   constructor () {
+//     super()
+//     this.setupListeners()
+//   }
 
-  /**
-   *  Start iterating from a given height.
-   */
-  private async setupListeners () {
-  }
+//   /**
+//    *  Start iterating from a given height.
+//    */
+//   private async setupListeners () {
+//   }
 
-  /**
-   *  Get the provider
-   */
-  getProvider () {
-  }
+//   /**
+//    *  Get the provider
+//    */
+//   getProvider () {
+//   }
 
-  /**
-   * Await initialization of the provider.
-   */
-  async awaitInitialized () {
-  }
+//   /**
+//    * Await initialization of the provider.
+//    */
+//   async awaitInitialized () {
+//   }
 
-  /**
-   * Start iterating events.
-   * @returns - An async generator that yields events
-   * @throws - Error if the source is not a ReadableStream or an AsyncIterable
-  //  * @yields T - The data read from the source
-   */
-  async * [Symbol.asyncIterator] (): AsyncGenerator<T> {
-    while (true) {
-      // if (this.eventQueue.length > 0) {
-      //   yield this.eventQueue.shift() as T
-      // } else {
-      //   // TODO: return undefined ONLY FOR TESTING
-      //   if (this.syncing) {
-      //     await new Promise(resolve => setTimeout(resolve, 1000)) // Wait for 1 second before checking again
-      //   } else { return }
-      // }
-    }
-  }
+//   /**
+//    * Start iterating events.
+//    * @returns - An async generator that yields events
+//    * @throws - Error if the source is not a ReadableStream or an AsyncIterable
+//   //  * @yields T - The data read from the source
+//    */
+//   async * [Symbol.asyncIterator] (): AsyncGenerator<T> {
+//     while (true) {
+//       // if (this.eventQueue.length > 0) {
+//       //   yield this.eventQueue.shift() as T
+//       // } else {
+//       //   // TODO: return undefined ONLY FOR TESTING
+//       //   if (this.syncing) {
+//       //     await new Promise(resolve => setTimeout(resolve, 1000)) // Wait for 1 second before checking again
+//       //   } else { return }
+//       // }
+//     }
+//   }
 
-  /**
-   *  Start iterating from a given height.
-   * @param options - Options for the iterator
-   * @param options.startBlock - The block number to start from
-   * @param options.endBlock - The block number to end at
-   * @returns - An async generator that yields events
-  //  * @yields T - The data read from the source
-   */
-  async * from (options:{ startBlock: bigint, endBlock: bigint }) {
-    // const { startBlock, endBlock } = options
-    // this.startBlock = BigInt(startBlock)
-    // this.lastScannedBlock = BigInt(endBlock)
-    // Logic to iterate from a given height
-    // const TOTAL_BLOCKS = BigInt(endBlock) - BigInt(startBlock)
-  }
+//   /**
+//    *  Start iterating from a given height.
+//    * @param options - Options for the iterator
+//    * @param options.startBlock - The block number to start from
+//    * @param options.endBlock - The block number to end at
+//    * @returns - An async generator that yields events
+//   //  * @yields T - The data read from the source
+//    */
+//   async * from (options:{ startBlock: bigint, endBlock: bigint }) {
+//     // const { startBlock, endBlock } = options
+//     // this.startBlock = BigInt(startBlock)
+//     // this.lastScannedBlock = BigInt(endBlock)
+//     // Logic to iterate from a given height
+//     // const TOTAL_BLOCKS = BigInt(endBlock) - BigInt(startBlock)
+//   }
 
-  /**
-   * Destroy the provider
-   * @returns - A promise that resolves when the provider is destroyed
-   */
-  async destroy () {
-    // Logic to destroy the provider and iterators
-    console.log('SubsquidProvider: Destroying provider')
-  }
-}
+//   /**
+//    * Destroy the provider
+//    * @returns - A promise that resolves when the provider is destroyed
+//    */
+//   async destroy () {
+//     // Logic to destroy the provider and iterators
+//     console.log('SubsquidProvider: Destroying provider')
+//   }
+// }
 
-export { SubsquidProvider, RAILGUN_SCAN_START_BLOCK, RAILGUN_SCAN_START_BLOCK_V2 }
+// export { SubsquidProvider, RAILGUN_SCAN_START_BLOCK, RAILGUN_SCAN_START_BLOCK_V2 }
