38d54fd558f02695c32a5f7ed4ba9ee495cb816c
Itachi546 <28756796+Itachi546@users.noreply.github.com>
2025-07-16 11:25:05 +0545

    chore: add logic in provider to fetch data

--- src/sources/subsquid/provider.ts
+++ src/sources/subsquid/provider.ts
@@ -1,5 +1,29 @@
+import { SubsquidClient } from '@railgun-reloaded/subsquid-client'
+
 import type { EVMBlock } from '../../models'
 import type { DataSource, SyncOptions } from '../data-source'
+
+import { autoPaginateBlockQuery } from './query'
+
+// Poll squid height every two minute
+const SQUID_HEIGHT_POLL_INTERVAL = 120_000
+
+type SubsquidEvmBlock = {
+  number: string;
+  hash: string;
+  timestamp: string;
+  transactions: {
+    hash: string;
+    index: string;
+    from: string;
+    logs: {
+      index: string
+      address: string
+      name: string
+      args: string
+    }[]
+  }[],
+}
 
 /**
  * Subsquid Provider for fetching data from Subsquid indexers
@@ -15,30 +39,105 @@ export class SubsquidProvider<T extends EVMBlock> implements DataSource<T> {
   /** Flag to indicate if the data-source can provide live data or not */
   isLiveProvider = false
 
+  /** Client to interact with subsquid */
+  #client: SubsquidClient
+
+  /**
+   * Field used to store the timeout which can be cleared later
+   */
+  #pollHeadTimeout: NodeJS.Timeout | null = null
+
   /**
    * Initialize Subsquid provider
    * @param endpoint - Subsquid endpoint URL
    */
   constructor (endpoint: string) {
-    // TODO: Implement Subsquid connection logic
-    console.log('Subsquid endpoint:', endpoint)
+    this.#client = new SubsquidClient({
+      customSubsquidUrl: endpoint
+    })
+
+    this.#pollHead()
+  }
+
+  /**
+   * Poll squid for latest height
+   */
+  async #pollHead () {
+    this.head = await this.#getBlockHeight()
+    if (this.#pollHeadTimeout) {
+      clearTimeout(this.#pollHeadTimeout)
+    }
+    this.#pollHeadTimeout = setTimeout(this.#pollHead, SQUID_HEIGHT_POLL_INTERVAL)
+  }
+
+  /**
+   * Get Block Height
+   * @returns Current block height indexed by subsquid
+   */
+  async #getBlockHeight () {
+    const { squidStatus } = await this.#client.query({
+      squidStatus: {
+        fields: ['height'],
+      }
+    })
+    if (!squidStatus?.height) throw new Error('Failed to get height from subsquid')
+
+    return BigInt(squidStatus.height)
   }
 
   /**
    * Create an async iterator with given config
    * @param _options - Sync options (unused in placeholder implementation)
    * @returns AsyncGenerator that returns RailgunTransactionData
+   * @yields EVMBlock - Block data with grouped/sorted transactions and logs
    */
   async * from (_options: SyncOptions): AsyncGenerator<T> {
-    // TODO: Implement Subsquid data fetching logic
-    throw new Error('Subsquid provider not yet implemented')
+    if (_options.liveSync) {
+      throw new Error("Subsquid doesn't support liveSync")
+    }
+
+    const result = await autoPaginateBlockQuery<SubsquidEvmBlock>(this.#client, _options.startHeight, _options.endHeight)
+
+    /**
+     * Convert subsquid data to standard format
+     * @param input - Subsquid representation of BlockData
+     * @returns Standard representation of EVMBlockData
+     */
+    const formatResult = (input : SubsquidEvmBlock) : EVMBlock => {
+      return {
+        number: BigInt(input.number),
+        hash: input.hash,
+        timestamp: BigInt(input.timestamp),
+        transactions: input.transactions.map(tx => ({
+          index: Number(tx.index),
+          hash: tx.hash,
+          from: tx.from,
+          logs: tx.logs.map((log) => (
+            {
+              index: Number(log.index),
+              name: log.name,
+              address: log.address,
+              args: {
+                ...JSON.parse(log.args as unknown as string)
+              }
+            }
+          ))
+        })),
+        internalTransaction: []
+      }
+    }
+
+    for (const entry of result) {
+      yield formatResult(entry) as T
+    }
   }
 
   /**
    * Destroy the Subsquid connection
    */
   destroy () {
-    // TODO: Implement Subsquid connection destruction logic
-    console.log('Subsquid connection destroyed')
+    if (this.#pollHeadTimeout) {
+      clearTimeout(this.#pollHeadTimeout)
+    }
   }
 }
